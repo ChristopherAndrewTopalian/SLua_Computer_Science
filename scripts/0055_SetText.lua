cf024845797b71982ac090c6347abbf0ee25f01c
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 19:49:38 -0400

    update

---
+++ scripts/0055_SetText.lua
@@ -0,0 +1,21 @@
+-- SetText.lua
+
+local ourMessage = "Hi Everyone"
+
+function state_entry()
+    ll.SetText(ourMessage, vector(0, 255, 255), 1)
+end
+
+state_entry()
+
+--[[
+Hi Everyone
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
---
+++ scripts/0056_SetText_tostring.lua
@@ -0,0 +1,21 @@
+-- SetText_tostring.lua
+
+local count = 0
+
+function state_entry()
+    ll.SetText(tostring(count), vector(0, 255, 255), 1)
+end
+
+state_entry()
+
+--[[
+0
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
