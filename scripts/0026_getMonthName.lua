d18478a021c952f7a3cd5a05fdb044dc433b4668
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 14:55:46 -0400

    update

---
+++ scripts/0026_getMonthName.lua
@@ -0,0 +1,36 @@
+-- getMonthName.lua
+
+function getMonthName()
+    local currentDate = ll.GetDate()
+    -- get characters from 6-7 for month
+    local monthNumber = tonumber(string.sub(currentDate, 6, 7))
+
+    local monthNames = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    }
+
+    return monthNames[monthNumber]
+end
+
+function state_entry()
+    ll.Say(0, getMonthName())
+end
+
+function touch_start()
+    ll.Say(0, getMonthName())
+end
+
+state_entry()
+
+--[[
+March
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
---
+++ scripts/0027getDayName.lua
@@ -0,0 +1,53 @@
+-- getDayName.lua
+
+function getDayName()
+    -- YYYY-MM-DD
+    local today = ll.GetDate()
+
+    -- days of the week
+    local daysOfWeek = {
+        ["Monday"] = 1,
+        ["Tuesday"] = 2,
+        ["Wednesday"] = 3,
+        ["Thursday"] = 4,
+        ["Friday"] = 5,
+        ["Saturday"] = 6,
+        ["Sunday"] = 7
+    }
+
+    -- get year, month and day from date
+    local year, month, day = string.match(today, "(%d+)-(%d+)-(%d+)")
+
+    -- convert extracted values to numbers
+    year = tonumber(year)
+    month = tonumber(month)
+    day = tonumber(day)
+
+    local dayNames = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+
+    local dayOfWeekIndex = (day + month + year) % 7
+
+    return dayNames[dayOfWeekIndex + 1]
+end
+
+function state_entry()
+    ll.Say(0, "Today is " .. getDayName())
+end
+
+function touch_start()
+    ll.Say(0, "Today is " .. getDayName())
+end
+
+state_entry()
+
+--[[
+Today is Friday
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
