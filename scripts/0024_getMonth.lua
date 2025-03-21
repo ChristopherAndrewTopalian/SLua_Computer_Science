d3afaf5bdb9abe056b6209f2533a90b91ae0f713
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 14:14:01 -0400

    update

---
+++ scripts/0024_getMonth.lua
@@ -0,0 +1,31 @@
+-- getMonth.lua
+
+function getMonth()
+    local currentDate = ll.GetDate()
+    -- get characters 6-7 for the month
+    local month = string.sub(currentDate, 6, 7)
+    return month
+end
+
+function state_entry()
+    ll.Say(0, getMonth())
+end
+
+function touch_start()
+    ll.Say(0, getMonth())
+end
+
+state_entry()
+
+--[[
+03
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
+
