850108065d06a6076789327aa27b2584bf6c2839
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 19:17:26 -0400

    update

---
+++ scripts/0054_DetectedName.lua
@@ -0,0 +1,24 @@
+-- DetectedName.lua
+
+function state_entry()
+   ll.Say(0, 'Touch to say your name')
+end
+
+function touch_start(total_number)
+    ll.Say(0, ll.DetectedName(0))
+end
+
+state_entry()
+
+--[[
+Touch to say your name
+Keystoner March
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
