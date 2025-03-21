2ccb428551d0bab14552aaa016dfbf11691d85f4
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 15:40:01 -0400

    update

---
+++ scripts/0029_getTime24PST.lua
@@ -0,0 +1,33 @@
+-- getTime24PST.lua
+
+function getTime24PST()
+    local timeInSeconds = ll.GetWallclock()
+    local hours = math.floor(timeInSeconds / 3600)
+    local minutes = math.floor((timeInSeconds % 3600) / 60)
+    local remainingSeconds = timeInSeconds % 60
+
+    -- HH:MM:SS format
+    return string.format("%02d:%02d:%02d", hours, minutes, remainingSeconds)
+end
+
+function state_entry()
+    ll.Say(0, "Current SL time: " .. getTime24PST())
+end
+
+function touch_start()
+    ll.Say(0, "Current SL time: " .. getTime24PST())
+end
+
+state_entry()
+
+--[[
+Current SL time: 12:37:22
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
