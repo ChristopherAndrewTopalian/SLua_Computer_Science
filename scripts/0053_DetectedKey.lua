b4177c6e9492c6cdc497b1a1ddbfed83ec2da0ba
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 19:10:15 -0400

    update

---
+++ scripts/0053_DetectedKey.lua
@@ -0,0 +1,24 @@
+-- DetectedKey.lua
+
+function state_entry()
+   ll.Say(0, 'Touch to find out your uuid key')
+end
+
+function touch_start(total_number)
+    ll.Say(0, ll.DetectedKey(0))
+end
+
+state_entry()
+
+--[[
+Touch to find out your uuid key
+0f6ab9e9-1738-469e-ab65-79200c433510
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
