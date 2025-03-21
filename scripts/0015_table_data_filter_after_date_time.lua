9f9f1da23ad96af4522b1a568dfa70cbd49b2cf4
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 13:18:18 -0400

    update

---
+++ scripts/0015_table_data_filter_after_date_time.lua
@@ -0,0 +1,35 @@
+-- table_data_filter_after_date_time.lua
+
+local people = {
+    {
+        name = "jane",
+        score = 98,
+        date = '1980/03/01 08:00'
+    },
+
+    {
+        name = "tabitha",
+        score = 93,
+        date = '1987/10/05 17:00'
+    }
+}
+
+function touch_start(total_number)
+    for i = 1, #people do
+        if people[i].date > '1980/03/01 08:50' then
+            ll.Say(0, people[i].name)
+        end
+    end
+end
+
+--[[
+tabitha
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
