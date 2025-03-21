91618e5877ee378665481f055a8e7da3c33775aa
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 19:58:47 -0400

    update

---
+++ scripts/0058_SetText_tostring_decrement_counter.lua
@@ -0,0 +1,27 @@
+-- SetText_tostring_decrement_counter.lua
+
+local count = 100
+
+function state_entry()
+    ll.SetText(tostring(count), vector(0, 255, 255), 1)
+end
+
+function touch_start(total_number)
+    count -= 1
+
+    ll.SetText(tostring(count), vector(0, 255, 255), 1)
+end
+
+state_entry()
+
+--[[
+100
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
