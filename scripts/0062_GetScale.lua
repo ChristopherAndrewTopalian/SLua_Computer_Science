e60534b78de7007715f5c92d00dec04830304165
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-21 21:21:33 -0400

    update

---
+++ scripts/0062_GetScale.lua
@@ -0,0 +1,29 @@
+-- GetScale.lua
+
+local currentScale;
+
+function state_entry()
+    currentScale = ll.GetScale()
+
+    ll.SetText(tostring(currentScale), vector(0, 255, 255), 1)
+end
+
+function touch_start(total_number)
+    currentScale = ll.GetScale()
+
+    ll.SetText(tostring(currentScale), vector(0, 255, 255), 1)
+end
+
+state_entry()
+
+--[[
+<0.50000, 0.50000, 0.50000>
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
