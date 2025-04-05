a0a68326009589c11461be4db684e3a8e2cb8398
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-04-05 04:35:08 -0400

    update

---
+++ scripts/0063_GetFreeMemory.lua
@@ -0,0 +1,29 @@
+-- GetFreeMemory.lua
+
+local freeMemory;
+
+function state_entry()
+    freeMemory = ll.GetFreeMemory()
+
+    ll.SetText(tostring(freeMemory), vector(0, 255, 255), 1)
+end
+
+function touch_start(total_number)
+    freeMemory = ll.GetFreeMemory()
+
+    ll.SetText(tostring(freeMemory), vector(0, 255, 255), 1)
+end
+
+state_entry()
+
+--[[
+16000
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
---
+++ scripts/Topalian_Combine_Files_One_Folder.bat
@@ -0,0 +1,9 @@
+:: Topalian_Combine_Files_One_Folder.bat
+
+@echo off
+copy *.lua main.lua
+
+:: Dedicated to God the Father
+:: All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+:: https://github.com/ChristopherTopalian
+:: https://github.com/ChristopherAndrewTopalian
