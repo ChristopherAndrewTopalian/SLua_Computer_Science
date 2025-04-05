267b2ebe24af0715e3b88233bbbecf23c964aa8e
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-04-05 14:22:14 -0400

    update

---
+++ scripts/0064_SetPos.lua
@@ -0,0 +1,28 @@
+-- SetPos.lua
+
+function state_entry()
+    local currentPos = ll.GetPos()
+
+    ll.SetText(tostring(currentPos), vector(0, 255, 255), 1)
+end
+
+function touch_start(total_number)
+    ll.SetPos(ll.GetPos() + vector(0, 0, 0.1))
+
+    ll.SetText(tostring(ll.GetPos()), vector(0, 255, 255), 1)
+end
+
+
+state_entry()
+
+--[[
+Each time the prim is left clicked it will go higher
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
