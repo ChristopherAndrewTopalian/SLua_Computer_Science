7d4073a094c2d2dad2cc3021d2d9ff2f3a3db38d
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-04-07 13:13:13 -0400

    update

---
+++ scripts/0070_getMinutes.lua
@@ -0,0 +1,34 @@
+-- getMinutes.lua
+
+function getMinutes()
+    local timestamp = ll.GetTimestamp()  -- format: "YYYY-MM-DDTHH:MM:SS.ffffffZ"
+    local minutes = ll.GetSubString(timestamp, 14, 15)
+
+    return minutes
+end
+
+function state_entry()
+    local minutes = getMinutes()  -- get the current minutes in SLT
+    ll.OwnerSay("Current minutes (SLT): " .. tostring(minutes))
+    ll.SetText(tostring(minutes), vector(0, 255, 255), 1)
+end
+
+function touch_start(total_number)
+    local minutes = getMinutes()  -- get the current minutes in SLT
+    ll.OwnerSay("Current minutes (PST): " .. tostring(minutes))
+    ll.SetText(tostring(minutes), vector(0, 255, 255), 1)
+end
+
+state_entry()
+
+--[[
+11
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
