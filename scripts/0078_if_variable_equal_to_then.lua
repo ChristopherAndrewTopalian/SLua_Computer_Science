49e572acf90fc0b2720341e006581b85bc336525
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-04-08 10:32:11 -0400

    update

---
+++ scripts/0078_if_variable_equal_to_then.lua
@@ -0,0 +1,22 @@
+-- if_variable_equal_to_then.lua
+
+function touch_start(total_number)
+    local ourNumber = 17
+
+    if ourNumber == 17 then
+        ll.Say(0, 'Number is 17')
+    end
+
+end
+
+--[[
+17
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
