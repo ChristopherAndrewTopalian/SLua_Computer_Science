0ea6f354a6c2b6817f34653a58b26ca03305f0cb
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-04-08 13:23:00 -0400

    update

---
+++ scripts/0080_if_elseif_else_variable_check.lua
@@ -0,0 +1,25 @@
+-- if_elseif_else_variable_check.lua
+
+function touch_start(total_number)
+    local ourNumber = 19
+
+    if ourNumber == 17 then
+        ll.Say(0, 'Number is 17')
+    elseif ourNumber == 18 then
+        ll.Say(0, 'Number is 18')
+    else
+        ll.Say(0, 'Number is NOT 17 or 18')
+    end
+end
+
+--[[
+Number is NOT 17 or 18
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
---
+++ scripts/0081_for_loop_count_to_5.lua
@@ -0,0 +1,24 @@
+-- for_loop_count_to_5.lua
+
+function touch_start(total_number)
+    -- start a for loop that counts from 1 to 5
+    for i = 1, 5 do
+        ll.Say(0, 'Count: ' .. tostring(i))
+    end
+end
+
+--[[
+Count: 1
+Count: 2
+Count: 3
+Count: 4
+Count: 5
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
