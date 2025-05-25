27abf3d399f649a46018f567e34c7c5377aaa000
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-05-25 11:46:22 -0400

    update

---
+++ scripts/0088_table_sort_by_score.lua
@@ -0,0 +1,45 @@
+-- table_sort_by_score.lua
+
+local people =
+{
+    {
+        name = "Jane",
+        score = 98
+    },
+    {
+        name = "Tabitha",
+        score = 90
+    },
+    {
+        name = "Jennifer",
+        score = 95
+    }
+}
+
+-- sort by score ascending
+table.sort(people,
+function(a, b)
+    return a.score < b.score
+end)
+
+function state_entry()
+    for i = 1, #people do
+        ll.Say(0, people[i].name .. ": " .. people[i].score)
+    end
+end
+
+state_entry()
+
+--[[
+Tabitha: 90
+Jennifer: 95
+Jane: 98
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2025
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
