31491cf1a0eb70ebc290fc1165e6df8375267d42
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-15 15:06:16 -0400

    update

---
+++ scripts/0101_table_key_lookup_show_name.lua
@@ -0,0 +1,36 @@
+-- table_key_lookup_show_name.lua
+
+local people =
+{
+    Jane = {
+        name = "Jane",
+        score = 98
+    },
+    Tabitha = {
+        name = "Tabitha",
+        score = 90
+    },
+    Jennifer = {
+        name = "Jennifer",
+        score = 95
+    }
+}
+
+function state_entry()
+    -- Instant lookup using dot notation
+    ll.Say(0, tostring(people.Jane.name))
+end
+
+state_entry()
+
+--[[
+Jane
+]]
+
+--[[
+Dedicated to God the Father
+All Rights Reserved Christopher Andrew Topalian Copyright 2026
+https://github.com/ChristopherTopalian
+https://github.com/ChristopherAndrewTopalian
+]]
+
